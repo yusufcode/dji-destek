e1c94b19a08175d724dfc5047b9f5f1339ebb897
yusufcode <yusuf1akbaba@gmail.com>
2021-06-28 17:10:41 +0300

    favorite blogs updated at the bottom

---
+++ models/Categories.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const Schema = new mongoose.Schema({
+    categoryName: {type:String},
+    categoryDetails: {type:String},
+    categoryImage: {type:String},
+    categoryStatus: {type:Boolean},
+    categoryGeneralStatus: {type:Boolean},
+    categorySeoText: {type:String}
+}, {timestamps:true})
+
+module.exports = mongoose.model('categories', Schema)
---
+++ models/Products.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const Schema = new mongoose.Schema({
+    categoryName: {type:String},
+    productName: {type:String},
+    productDetails: {type:String},
+    productImage: {type:String},
+    productPrice: {type:String},
+    productStatus: {type:Boolean},
+    productGeneralStatus: {type:Boolean},
+    productSeoText: {type:String}
+}, {timestamps:true})
+
+module.exports = mongoose.model('products', Schema)
