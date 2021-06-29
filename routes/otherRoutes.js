2490278f43cae9ca1cfee36967563f321e0c8bec
yusufcode <yusuf1akbaba@gmail.com>
2021-06-29 17:03:02 +0300

    404 page added favoriteBlogs

--- routes/otherRoutes.js
+++ routes/otherRoutes.js
@@ -3,17 +3,22 @@ const router = express.Router()
 const fs = require("fs");
 
 const Pages = require('../models/Pages')
+const Blogs = require('../models/Blogs')
 
-router.get('/404', (req, res) => {
-    Pages.findOne({page: "404"}, (err, page) => {
-        res.render(page.page, { title: page.title, description: page.description, keywords: page.keywords, author: page.author, bodyClass: page.bodyClass, seoText: '' })
-    })
+
+router.get('/404', async (req, res) => {
+
+    const page = await Pages.findOne({page: "404"})
+    const favoriteBlogs = await Blogs.find({blogStatus: true}).sort({ _id : -1 }).limit(5)
+    res.render(page.page, { title: page.title, description: page.description, keywords: page.keywords, author: page.author, bodyClass: page.bodyClass, seoText: '', favoriteBlogs: favoriteBlogs})
+    
 })
 
-router.get('/admin/404', (req, res) => {
-    Pages.findOne({page: "404"}, (err, page) => {
-        res.render(page.page, { title: page.title, description: page.description, keywords: page.keywords, author: page.author, bodyClass: page.bodyClass, layout: 'layouts/layout-admin', pageName: '404' })
-    })
+router.get('/admin/404', async (req, res) => {
+    
+    const page = await Pages.findOne({page: "404"})
+    res.render(page.page, { title: page.title, description: page.description, keywords: page.keywords, author: page.author, bodyClass: page.bodyClass, layout: 'layouts/layout-admin', pageName: '404' })
+    
 })
 
 router.get('/sitemap.xml', (req, res) => {
